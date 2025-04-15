47cc03ee27a58e0da1d40f09a70de709fe315130
Oliver <145952622+GummyHat@users.noreply.github.com>
2025-04-15 16:48:16 -0400

    Update TDES.h

--- TDES.h
+++ TDES.h
@@ -4,8 +4,12 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <vector>
 #include <iomanip>
 
+std::bitset<64> Bytes_To_Bitset(const unsigned char* bytes);
+void Bitset_To_Bytes(const std::bitset<64>& bits, unsigned char* bytes);
+int TDES_Encrypt_Bytes(unsigned char* ciphertext_output, const unsigned char* plaintext, int plaintext_length, const unsigned char* key24bytes);
 std::bitset<64> generate_Ciphertext(const std::bitset<64>& plaintext, const std::bitset<128>& KeyX, const std::bitset<128> KeyY);
 std::bitset<64> DES_Encrypt(const std::bitset<64>& plaintext, const std::bitset<48> subkeys[16]);
 std::bitset<64> DES_Decrypt(const std::bitset<64>& ciphertext, const std::bitset<48> subkeys[16]);
@@ -17,8 +21,9 @@ std::bitset<56> mergeKeys(const std::bitset<28>& left, const std::bitset<28>& ri
 std::bitset<28> leftCircularBitShift(const std::bitset<28>& input, const size_t &shifts);
 std::bitset<56> PC_Function(const std::bitset<64>& key);
 std::bitset<48> PC2_Function(const std::bitset<56>& key);
-std::string Hex_To_Binary(const std::string& hex);
-std::string Binary_To_Hex(const std::string& binary);
+std::bitset<64> Hex_To_Binary(const std::string& hex);
+std::string Binary_To_Hex(const std::bitset<64>& binary);
+
 
 
 #endif //TDES_H
